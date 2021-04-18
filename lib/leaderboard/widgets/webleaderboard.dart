e7f7a74297ce9aaa788790ab5e6ca5c8a6902eff
Karthikeyan S <karthikeyanssvk@gmail.com>
2021-04-18 22:23:14 +0530

    refactor: leaderboard(web) & made UI changes

--- lib/leaderboard/widgets/webleaderboard.dart
+++ lib/leaderboard/widgets/webleaderboard.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:vegas_lit/config/palette.dart';
 import 'package:vegas_lit/config/styles.dart';
 import 'package:vegas_lit/leaderboard/models/player.dart';
+import 'package:vegas_lit/leaderboard/widgets/pagenumberview.dart';
+import 'package:vegas_lit/leaderboard/widgets/textbar.dart';
 
 class WebLeaderboard extends StatefulWidget {
   final List<LeaderBoardPlayer> players;
@@ -13,64 +15,105 @@ class WebLeaderboard extends StatefulWidget {
 class _WebLeaderboardState extends State<WebLeaderboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 20.0),
-      decoration: BoxDecoration(
-          border: Border.all(
-            color: Palette.cream,
-          ),
-          borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          Container(
-            constraints: const BoxConstraints(maxWidth: 1220),
-            height: 80,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8), topRight: Radius.circular(8)),
-              color: Palette.darkGrey,
-            ),
-            child: Center(
-              child: Text(
-                'CONTEST WINNINGS',
-                style: Styles.largeTextBold,
-              ),
-            ),
-          ),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 1220),
-            height: 8,
-            color: Palette.green,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: 1220),
-              child: Column(
-                children: [
-                  LeaderboardColumns(),
-                  Column(
-                    children: widget.players
-                        .map((player) => WebLeaderboardItem(
-                              player: player,
-                            ))
-                        .toList(),
+    return Column(
+      children: [
+        Container(
+          constraints: const BoxConstraints(maxWidth: 1200),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    'Current Leaderboard',
+                    style: Styles.normalTextBold,
                   ),
-                  Container(
-                    height: 8,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(8),
-                          bottomRight: Radius.circular(8)),
-                      color: Palette.lightGrey,
-                    ),
-                  )
-                ],
-              ),
+                ),
+                Expanded(child: Container()),
+                const TextBar(
+                  text: 'Current Week',
+                ),
+                const TextBar(
+                  text: 'All Leagues',
+                ),
+                const TextBar(
+                  text: 'All Bet Types',
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 20.0),
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Palette.cream,
+              ),
+              borderRadius: BorderRadius.circular(8)),
+          child: Column(
+            children: [
+              Container(
+                constraints: const BoxConstraints(maxWidth: 1220),
+                height: 80,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8)),
+                  color: Palette.darkGrey,
+                ),
+                child: Center(
+                  child: Text(
+                    'CONTEST WINNINGS',
+                    style: Styles.largeTextBold,
+                  ),
+                ),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 1220),
+                height: 8,
+                color: Palette.green,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  constraints: const BoxConstraints(maxWidth: 1220),
+                  child: Column(
+                    children: [
+                      LeaderboardColumns(),
+                      Column(
+                        children: widget.players
+                            .map((player) => WebLeaderboardItem(
+                                  player: player,
+                                ))
+                            .toList(),
+                      ),
+                      Container(
+                        height: 8,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(8),
+                              bottomRight: Radius.circular(8)),
+                          color: Palette.lightGrey,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: PageNumberView(pages: 15),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
