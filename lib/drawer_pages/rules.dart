20108ad88c08eee2fab7ee91640db0479615383d
Ritik Gupta <shaan.xaz@gmail.com>
2021-04-22 01:55:05 +0530

    fix(drawer): empty page text

--- lib/drawer_pages/rules.dart
+++ lib/drawer_pages/rules.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:vegas_lit/config/assets.dart';
+import 'package:vegas_lit/config/palette.dart';
 import 'package:vegas_lit/config/styles.dart';
 
 class Rules extends StatelessWidget {
@@ -34,6 +36,19 @@ class Rules extends StatelessWidget {
                 style: Styles.pageTitle,
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 120),
+            child: Text(
+              // ignore: lines_longer_than_80_chars
+              'WORK IN PROGRESS\n Come Back Later.',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.nunito(
+                color: Palette.cream,
+                fontSize: 22,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
           ),
         ],
       ),
