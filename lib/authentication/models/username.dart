bb63eb48033bcfe8f86902887e6e027633a81159
Ritik <shaan.xaz@gmail.com>
2021-05-23 07:49:12 +0530

    add(sign_up): username validation

--- lib/authentication/models/username.dart
+++ lib/authentication/models/username.dart
@@ -3,9 +3,13 @@ import 'package:formz/formz.dart';
 enum UsernameValidationError { invalid, empty, exist }
 
 class Username extends FormzInput<String, UsernameValidationError> {
-  const Username.pure({this.externalError}) : super.pure('username');
+  const Username.pure({this.externalError}) : super.pure('username12');
   const Username.dirty(this.externalError, [String value = ''])
       : super.dirty(value);
+
+  // static final RegExp _usernameRegExp = RegExp(
+  //   r'^[a-zA-Z0-9]+$',
+  // );
 
   final UsernameValidationError externalError;
 
@@ -13,7 +17,11 @@ class Username extends FormzInput<String, UsernameValidationError> {
   UsernameValidationError validator(String value) {
     if (externalError != null) {
       return externalError;
-    } else if (value.length <= 15 && value.length >= 5) {
+    }
+    //  else if (_usernameRegExp.hasMatch(value)) {
+    //   return null;
+    // }
+    else if (value.length <= 15 && value.length >= 5) {
       return null;
     } else if (value.isEmpty) {
       return UsernameValidationError.empty;
