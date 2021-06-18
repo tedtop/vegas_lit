d8ca9227c48a108157ac4ba0de777281f4367f58
Ritik <shaan.xaz@gmail.com>
2021-06-18 08:33:43 +0530

    add(cloud): week field change  function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,12 @@ import * as slack from "./cloud_functions/slack";
 import * as bets from "./cloud_functions/bets";
 import * as leaderboard from "./cloud_functions/leaderboard";
 import * as contestWeek from "./cloud_functions/contest_week";
+import * as bet from "./cloud_functions/temporary_bet_field";
 
 admin.initializeApp();
 
-export const sendBetCreatedToSlack= slack.sendBetCreatedToSlack;
-export const resolveBets= bets.resolveBets;
+export const sendBetCreatedToSlack = slack.sendBetCreatedToSlack;
+export const resolveBets = bets.resolveBets;
 export const rankLeaderboard = leaderboard.rankLeaderboard;
 export const resetContestWeek = contestWeek.resetContestWeek;
+export const betField = bet.betField;
