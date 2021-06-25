b7037f0dc7adcb66a4fd3283de05ac00d1d5e6d8
Ritik <shaan.xaz@gmail.com>
2021-06-26 02:53:13 +0530

    chore(cloud): commenting out unused function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import * as slack from "./cloud_functions/slack";
 import * as bets from "./cloud_functions/bets";
 // import * as leaderboard from "./cloud_functions/leaderboard";
 import * as contestWeek from "./cloud_functions/contest_week";
-import * as bet from "./cloud_functions/temporary_bet_field";
+// import * as bet from "./cloud_functions/temporary_bet_field";
 
 admin.initializeApp();
 
@@ -12,4 +12,4 @@ export const sendBetCreatedToSlack = slack.sendBetCreatedToSlack;
 export const resolveBets = bets.resolveBets;
 // export const rankLeaderboard = leaderboard.rankLeaderboard;
 export const resetContestWeek = contestWeek.resetContestWeek;
-export const betField = bet.betField;
+// export const betField = bet.betField;
