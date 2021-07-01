682df56daa1d0c1c2e2dff8cc123376781371d1e
Ritik <shaan.xaz@gmail.com>
2021-07-01 18:31:02 +0530

    add(cloud): rank previous leaderboard

--- functions/src/cloud_functions/leaderboard.ts
+++ functions/src/cloud_functions/leaderboard.ts
@@ -1,40 +1,71 @@
-// import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// export const rankLeaderboard = functions.pubsub
-//   .schedule("1 * * * *")
-//   .timeZone("America/New_York")
-//   .onRun(async (context) => {
-//     await admin
-//       .firestore()
-//       .collection("wallets")
-//       .where("totalBets", ">=", 5)
-//       .get()
-//       .then(async function (snapshots) {
-//         let rankNumber = 1;
-//         const documents = snapshots.docs.sort((a, b) => {
-//           const firstData = a.data();
-//           const secondData = b.data();
-//           if (
-//             firstData.accountBalance + firstData.pendingRiskedAmount >
-//             secondData.accountBalance + secondData.pendingRiskedAmount
-//           )
-//             return -1;
-//           if (
-//             firstData.accountBalance + firstData.pendingRiskedAmount <
-//             secondData.accountBalance + secondData.pendingRiskedAmount
-//           )
-//             return 1;
-//           if (firstData.totalBets > secondData.totalBets) return -1;
-//           if (firstData.totalBets < secondData.totalBets) return 1;
-//           return 0;
-//         });
-//         for (const document of documents) {
-//           await document.ref.update({ rank: rankNumber });
-//           rankNumber++;
-//         }
-//       })
-//       .catch(function (error: any) {
-//         console.log(error);
-//       });
-//   });
+export const rankPreviousWeekLeaderboard = functions.pubsub
+  .schedule("0 */3 * * THU")
+  .timeZone("America/New_York")
+  .onRun(async (context) => {
+    const list = await admin
+      .firestore()
+      .collection("leaderboard")
+      .doc("global")
+      .collection("weeks")
+      .get()
+      .then(async (snapshots) => {
+        return await Promise.all(
+          snapshots.docs.map((element) => {
+            return element.id;
+          })
+        );
+      });
+
+    await Promise.all(
+      list.map(async (week) => {
+        await admin
+          .firestore()
+          .collection("leaderboard")
+          .doc("global")
+          .collection("weeks")
+          .doc(week)
+          .collection("wallets")
+          .where("totalBets", ">=", 5)
+          .get()
+          .then(async function (snapshots) {
+            let rankNumber = 1;
+            const documents = snapshots.docs.sort((a, b) => {
+              const firstData = a.data();
+              const secondData = b.data();
+              if (
+                firstData.accountBalance +
+                  firstData.pendingRiskedAmount -
+                  firstData.totalRewards >
+                secondData.accountBalance +
+                  secondData.pendingRiskedAmount -
+                  secondData.totalRewards
+              )
+                return -1;
+              if (
+                firstData.accountBalance +
+                  firstData.pendingRiskedAmount -
+                  firstData.totalRewards <
+                secondData.accountBalance +
+                  secondData.pendingRiskedAmount -
+                  secondData.totalRewards
+              )
+                return 1;
+              if (firstData.totalBets > secondData.totalBets) return -1;
+              if (firstData.totalBets < secondData.totalBets) return 1;
+              return 0;
+            });
+            for (const document of documents) {
+              await document.ref.update({ rank: rankNumber });
+              rankNumber++;
+            }
+          })
+          .catch(function (error: any) {
+            console.log(error);
+          });
+      })
+    );
+    return true;
+  });
