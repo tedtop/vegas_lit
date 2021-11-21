a2ce08cd73ea9bd3a2ce40fe3f23b249430d88e5
Karthikeyan S <karthikeyanssvk@gmail.com>
2021-11-21 14:24:35 +0530

    change(stats): mlb team stats ui change

--- lib/features/games/baseball/mlb/views/player_details/player_details.dart
+++ lib/features/games/baseball/mlb/views/player_details/player_details.dart
@@ -60,7 +60,8 @@ class PlayerDetailsPage extends StatelessWidget {
           BlocBuilder<PlayerDetailsCubit, PlayerDetailsState>(
             builder: (context, state) {
               if (state is PlayerDetailsOpened) {
-                return StatsBox(statMap: state.playerStats.toStatOnlyMap());
+                return SizedBox.shrink();
+                //StatsBox(statMap: state.playerStats.toStatOnlyMap());
               } else {
                 return const Padding(
                   padding: EdgeInsets.all(20),
@@ -249,74 +250,101 @@ class PlayerDetailsPage extends StatelessWidget {
 }
 
 class StatsBox extends StatelessWidget {
-  const StatsBox({Key? key, required this.statMap}) : super(key: key);
-  final Map<String, dynamic> statMap;
-
-  List<Widget> _statMapToList() {
-    return statMap.keys.map(
-      (key) {
-        if (statMap[key] != null && statMap[key] != 0) {
-          return StatsText(
-            leftText: key,
-            rightText: statMap[key],
-          );
-        } else {
-          return const SizedBox();
-        }
-      },
-    ).toList();
-  }
+  const StatsBox(
+      {Key? key, required this.hittingMap, required this.pitchingMap})
+      : super(key: key);
+  final Map<String, dynamic> hittingMap, pitchingMap;
 
   @override
   Widget build(BuildContext context) {
-    final statsList = _statMapToList();
-    final statsOffset = (statsList.length ~/ 2) + 8;
     return Container(
-      width: 380,
-      margin: const EdgeInsets.only(top: 20, bottom: 8),
-      padding: const EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 22),
-      decoration: BoxDecoration(
-          color: Palette.lightGrey,
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-          border: Border.all(
-            color: Palette.cream,
-          )),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 170,
-            child: Column(
-              children: statsList.sublist(0, statsOffset),
+        width: 380,
+        margin: const EdgeInsets.only(top: 20, bottom: 8),
+        padding: const EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 22),
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            border: Border.all(
+              color: Palette.cream,
+            )),
+        child: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            backgroundColor: Palette.lightGrey,
+            appBar: AppBar(
+              bottom: TabBar(
+                indicatorColor: Palette.green,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'HITTING',
+                      style: Styles.normalText,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'PITCHING',
+                      style: Styles.normalText,
+                    ),
+                  ),
+                ],
+              ),
             ),
+            body: TabBarView(children: [
+              Builder(
+                builder: (context) {
+                  List<Widget> statPieces = [];
+                  hittingMap.entries.toList().asMap().forEach((index, map) {
+                    if (index == 2 || index == 12)
+                      statPieces.add(VerticalDivider(
+                        color: Palette.cream,
+                      ));
+                    statPieces.add(StatsText(
+                        abbrText: map.key, valText: map.value.toString()));
+                  });
+                  return Column(children: [
+                    Row(children: statPieces.sublist(0, 9)),
+                    Divider(
+                      color: Palette.cream,
+                    ),
+                    Row(children: statPieces.sublist(9)),
+                  ]);
+                },
+              ),
+              // change it for pitching
+              Builder(
+                builder: (context) {
+                  List<Widget> statPieces = [];
+                  hittingMap.entries.toList().asMap().forEach((index, map) {
+                    if (index == 2 || index == 12)
+                      statPieces.add(VerticalDivider(
+                        color: Palette.cream,
+                      ));
+                    statPieces.add(StatsText(
+                        abbrText: map.key, valText: map.value.toString()));
+                  });
+                  return Column(children: [
+                    Row(children: statPieces.sublist(0, 9)),
+                    Divider(
+                      color: Palette.cream,
+                    ),
+                    Row(children: statPieces.sublist(9)),
+                  ]);
+                },
+              ),
+            ]),
           ),
-          const SizedBox(width: 15),
-          Expanded(
-              child: Column(
-            children: statsList.sublist(statsOffset),
-          ))
-        ],
-      ),
-    );
+        ));
   }
 }
 
 class StatsText extends StatelessWidget {
-  const StatsText({Key? key, this.leftText, this.rightText}) : super(key: key);
-  final String? leftText;
-  final dynamic rightText;
+  const StatsText({Key? key, required this.abbrText, required this.valText})
+      : super(key: key);
+  final String abbrText, valText;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(
-            width: 110, child: Text(leftText!, style: Styles.teamStatsText)),
-        Expanded(
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: Text('$rightText', style: Styles.teamStatsText))),
-      ],
+    return Column(
+      children: [Text(abbrText), Text(valText)],
     );
   }
 }
