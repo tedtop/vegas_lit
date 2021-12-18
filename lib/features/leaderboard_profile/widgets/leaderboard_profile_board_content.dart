442d8c44f78fd1baf929a5ddc1d5466d324200a1
Ritik <shaan.xaz@gmail.com>
2021-12-19 01:33:02 +0530

    fix(leaderboard_profile): board content ui change

--- lib/features/leaderboard_profile/widgets/leaderboard_profile_board_content.dart
+++ lib/features/leaderboard_profile/widgets/leaderboard_profile_board_content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vegas_lit/config/styles.dart';
+import 'package:vegas_lit/features/bet_history/widgets/bet_history_board_items.dart';
 
 import '../../../config/palette.dart';
 import '../leaderboard_profile.dart';
@@ -12,116 +14,120 @@ class LeaderboardProfileBoardContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final userWallet = context
         .select((LeaderboardProfileCubit cubit) => cubit.state.userWallet!);
-    return Row(
-      children: [
-        SizedBox(
-          width: 165,
-          child: Padding(
-            padding:
-                const EdgeInsets.only(top: 10, bottom: 10, left: 8, right: 2),
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              color: Palette.lightGrey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 8,
-                ),
-                child: Column(
-                  children: [
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Rank',
-                      rightText: userWallet.rank == 0
-                          ? 'N/A'
-                          : userWallet.rank!.ordinalNumber,
-                    ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Won',
-                      rightText: '${userWallet.totalBetsWon}',
-                    ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Lost',
-                      rightText: '${userWallet.totalBetsLost}',
-                    ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Open',
-                      rightText:
-                          '${userWallet.totalOpenBets! < 0 ? 0 : userWallet.totalOpenBets}',
-                    ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Cancelled',
-                      rightText:
-                          '${userWallet.totalBets! - (userWallet.totalBetsWon! + userWallet.totalBetsLost! + userWallet.totalOpenBets!)}',
-                    ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Total',
-                      rightText: '${userWallet.totalBets}',
-                    ),
-                  ],
-                ),
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      color: Palette.lightGrey,
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            height: 35,
+            width: double.infinity,
+            decoration: const BoxDecoration(color: Palette.cream),
+            child: Center(
+              child: Text(
+                'Rank ${userWallet.rank}',
+                style: Styles.homeTeam
+                    .copyWith(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding:
-                const EdgeInsets.only(top: 10, bottom: 10, left: 2, right: 8),
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              color: Palette.lightGrey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 8,
-                ),
-                child: Column(
-                  children: [
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Total Bet',
-                      rightText: '${userWallet.totalRiskedAmount}',
+          Row(
+            children: [
+              SizedBox(
+                width: 165,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 4, bottom: 6, left: 8, right: 2),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 8,
                     ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Total Won',
-                      rightText:
-                          '${userWallet.totalRiskedAmount! + userWallet.totalProfit! - userWallet.totalLoss! - userWallet.pendingRiskedAmount!}',
+                    child: Column(
+                      children: [
+                        BetHistoryBoardText(
+                          leftText: 'Won',
+                          rightText: '${userWallet.totalBetsWon}',
+                        ),
+                        BetHistoryBoardText(
+                          leftText: 'Lost',
+                          rightText: '${userWallet.totalBetsLost}',
+                        ),
+                        BetHistoryBoardText(
+                          leftText: 'Open',
+                          rightText:
+                              '${userWallet.totalOpenBets! < 0 ? 0 : userWallet.totalOpenBets}',
+                        ),
+                        BetHistoryBoardText(
+                          leftText: 'Cancelled',
+                          rightText:
+                              '${userWallet.totalBets! - (userWallet.totalBetsWon! + userWallet.totalBetsLost! + userWallet.totalOpenBets!)}',
+                        ),
+                        BetHistoryBoardText(
+                          leftText: 'Total',
+                          rightText: '${userWallet.totalBets}',
+                        ),
+                      ],
                     ),
-                    // LeaderboardProfileHistoryBoardText(
-                    //   leftText: 'Biggest Win',
-                    //   rightText: '${userWallet.biggestWinAmount}',
-                    // ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Ad Rewards',
-                      rightText: '${userWallet.totalRewards}',
-                    ),
-                    //  Should replace with biggest win
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Total Profit',
-                      rightText: '${userWallet.totalProfit}',
-                    ),
-                    // LeaderboardProfileHistoryBoardText(
-                    //   leftText: 'Total Refund',
-                    //   rightText:
-                    //       '${(userWallet.totalRiskedAmount + userWallet.totalProfit - userWallet.totalLoss - userWallet.pendingRiskedAmount) - (userWallet.totalRiskedAmount - userWallet.pendingRiskedAmount)}',
-                    // ),
-                    LeaderboardProfileHistoryBoardText(
-                      leftText: 'Balance',
-                      rightText: '${userWallet.accountBalance}',
-                      color: Palette.green,
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 4, bottom: 6, left: 2, right: 8),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 8,
+                    ),
+                    child: Column(
+                      children: [
+                        BetHistoryBoardText(
+                          leftText: 'Total Bet',
+                          rightText: '${userWallet.totalRiskedAmount}',
+                        ),
+                        BetHistoryBoardText(
+                          leftText: 'Total Won',
+                          rightText:
+                              '${userWallet.totalRiskedAmount! + userWallet.totalProfit! - userWallet.totalLoss! - userWallet.pendingRiskedAmount!}',
+                        ),
+                        // BetHistoryBoardText(
+                        //   leftText: 'Biggest Win',
+                        //   rightText: '${userWallet.biggestWinAmount}',
+                        // ),
+                        BetHistoryBoardText(
+                          leftText: 'Ad Rewards',
+                          rightText: '${userWallet.totalRewards}',
+                        ),
+                        //  Should replace with biggest win
+                        BetHistoryBoardText(
+                          leftText: 'Total Profit',
+                          rightText: '${userWallet.totalProfit}',
+                        ),
+                        // BetHistoryBoardText(
+                        //   leftText: 'Total Refund',
+                        //   rightText:
+                        //       '${userWallet.totalRiskedAmount - userWallet.totalRewards - (userWallet.totalRiskedAmount - userWallet.totalLoss - userWallet.pendingRiskedAmount) - 1000}',
+                        // ),
+                        BetHistoryBoardText(
+                          leftText: 'Balance',
+                          rightText: '${userWallet.accountBalance}',
+                          color: Palette.green,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
